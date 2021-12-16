113fe3c444307b0322b0357ead13a598368faf44
Mitya Kim <mitya_kim@icloud.com>
2021-12-16 13:17:38 -0800

    rollbar

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ const express = require('express')
 
 const path = require('path')
 
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '0e0cf7cb199b4e1981b15db83c990447',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
+
 const app = express()
 
 app.use(express.json())
@@ -9,6 +19,8 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 })
+
+app.use(rollbar.errorHandler())
 
 const port = process.env.PORT || 4500
 
